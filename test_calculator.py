3adf286b2d76571e68fdc86169e1e4fe10dff247
nostgoth <painter_nost@hotmail.com>
2025-03-12 21:22:56 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calc_phyton import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
