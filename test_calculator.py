38c14316b961a685b3e3329dd4e7951dd6bd2e60
nostgoth <painter_nost@hotmail.com>
2025-03-12 21:24:12 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 2), 4)
     
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(5, 3), 1)
     
     def test_multiply(self):
         self.assertEqual(multiply(4, 3), 12)
